5b35547eb7d0d59e1f2d96f22aacd01d41c1f095
mahumt <mahum.tofiq@outlook.com>
2024-04-06 17:27:03 +0500

    created images for zookeeper, kafka & spark

---
+++ 1.py
@@ -0,0 +1,75 @@
+def generate_vehicle_data(device_id):
+    location = simulate_vehicle_movement()
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': get_next_time().isoformat(),
+        'location': (location['latitude'], location['longitude']),
+        'speed': random.uniform(10, 40),
+        'direction': 'North-East',
+        'make': 'BMW',
+        'model': 'C500',
+        'year': 2024,
+        'fuelType': 'Hybrid'
+    }
+
+def generate_gps_data(device_id, timestamp, vehicle_type='private'):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': timestamp,
+        'speed': random.uniform(0, 40),  # km/h
+        'direction': 'North-East',
+        'vehicleType': vehicle_type
+    }
+
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'cameraId': camera_id,
+        'location': location,
+        'timestamp': timestamp,
+        'snapshot': 'Base64EncodedString'
+    }
+
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'location': location,
+        'timestamp': timestamp,
+        'temperature': random.uniform(-5, 26),
+        'weatherCondition': random.choice(['Sunny', 'Cloudy', 'Rain', 'Snow']),
+        'precipitation': random.uniform(0, 25),
+        'windSpeed': random.uniform(0, 100),
+        'humidity': random.randint(0, 100),  # percentage
+        'airQualityIndex': random.uniform(0, 500)  # AQL Value goes here
+    }
+
+def generate_emergency_incident_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'incidentId': uuid.uuid4(),
+        'type': random.choice(['Accident', 'Fire', 'Medical', 'Police', 'None']),
+        'timestamp': timestamp,
+        'location': location,
+        'status': random.choice(['Active', 'Resolved']),
+        'description': 'Description of the incident'
+    }
+
+def produce_data_to_kafka(producer, topic, data):
+    producer.produce(
+        topic,
+        key=str(data['id']),
+        value=json.dumps(data, default=json_serializer).encode('utf-8'),
+        on_delivery=delivery_report
+    )
+    producer.flush()
+
+produce_data_to_kafka(producer, VEHICLE_TOPIC, vehicle_data)
+produce_data_to_kafka(producer, GPS_TOPIC, gps_data)
+produce_data_to_kafka(producer, TRAFFIC_TOPIC, traffic_camera_data)
+produce_data_to_kafka(producer, WEATHER_TOPIC, weather_data)
+produce_data_to_kafka(producer, EMERGENCY_TOPIC, emergency_incident_data)
--- Readme.md
+++ Readme.md
@@ -1,3 +1,15 @@
+<!-- https://freedium.cfd/https://blog.stackademic.com/building-a-smart-city-an-end-to-end-big-data-engineering-project-7a3d9a6ab104 -->
+
 Use python 3.9  (download 3.9 to set up virtual env). 3.12 is finicky 
 Create virtual environment: `py -3.10 -m virtualenv smartcity_env` 
 Create docker-compose file
+
+
+## To push to github
+```
+git remote -v
+git remote add origin https://github.com/<user>/<user_repository>
+git add .
+git commit -m "updates to schema and sql files"
+git push origin main
+```
